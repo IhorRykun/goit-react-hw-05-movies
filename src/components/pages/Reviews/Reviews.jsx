6eaae1d72cb053f52648762c9f963fa84dfb30ee
Ihor <ihorrykun@gmail.com>
2023-02-25 11:20:45 +0200

    update page

---
+++ src/components/pages/Reviews/Reviews.jsx
@@ -0,0 +1,32 @@
+import { useParams } from 'react-router';
+import { useEffect, useState } from 'react';
+import { FetchMovieID } from 'components/API/Api';
+
+export const Reviews = () => {
+  const [reviews, setReviews] = useState([]);
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    FetchMovieID(movieId)
+      .then(movie => setReviews(movie.results))
+      .catch(error => console.log('error'));
+  }, [movieId]);
+  if (reviews.length === 0) {
+    return <p>Reviews not found</p>;
+  }
+
+  return (
+    <div>
+      <ul>
+        {reviews.map(({ id, author, content }) => {
+          return (
+            <li key={id}>
+              <h3>Author: {author}</h3>
+              <p>{content}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
