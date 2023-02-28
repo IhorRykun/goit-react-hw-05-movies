f256690004c2ed02abb3a4fee965656e28be204d
Ihor <ihorrykun@gmail.com>
2023-02-28 20:30:00 +0200

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,19 @@
 import { useState, useEffect } from 'react';
 import { FetchMovieTrend } from '../../components/API/Api';
 import { useLocation } from 'react-router-dom';
-import { ItemStyle, ListStyle, StyleLink } from './Home.styled';
+import {
+  ImgStyle,
+  ItemStyle,
+  ListStyle,
+  StyleLink,
+  TextContent,
+  TextTitle,
+} from './Home.styled';
 
 export const Home = () => {
   const [movieTrends, setMovieTrends] = useState([]);
- const location = useLocation();
-//  const []
-// const poster = 'https://image.tmdb.org/t/p/w500' + backdrop_path;
- 
- 
+  const location = useLocation();
+
   useEffect(() => {
     FetchMovieTrend(1)
       .then(gallery => {
@@ -20,24 +24,19 @@ export const Home = () => {
 
   return (
     <div>
-      <h1>Tranding Day</h1>
+      <TextTitle>Tranding Day</TextTitle>
       <ListStyle>
-        {movieTrends.map(
-          ({
-            id,
-            title,
-            path = 'https://image.tmdb.org/t/p/w500/130H1gap9lFfiTF9iDrqNIkFvC9.jpg',
-          }) => {
-            return (
-              <ItemStyle key={id}>
-                <img src={path} alt={title} />
-                <StyleLink to={`/movies/${id}`} state={{ from: location }}>
-                  {title}
-                </StyleLink>
-              </ItemStyle>
-            );
-          }
-        )}
+        {movieTrends.map(({ id, title, backdrop_path }) => {
+          const posters = 'https://image.tmdb.org/t/p/w500' + backdrop_path;
+          return (
+            <ItemStyle key={id}>
+              <StyleLink to={`/movies/${id}`} state={{ from: location }}>
+                <ImgStyle src={`${posters}`} alt={title} />
+                <TextContent>{`${title}`}</TextContent>
+              </StyleLink>
+            </ItemStyle>
+          );
+        })}
       </ListStyle>
     </div>
   );
--- src/pages/Home/Home.styled.jsx
+++ src/pages/Home/Home.styled.jsx
@@ -1,15 +1,30 @@
 import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
 
+export const TextTitle = styled.h1`
+  text-align: center;
+`;
 export const ListStyle = styled.ul`
   list-style: none;
+  display: flex;
+  flex-wrap: wrap;
 `;
 
 export const ItemStyle = styled.li`
   padding: 3px;
+  gap: 10px;
 `;
 
 export const StyleLink = styled(NavLink)`
   text-decoration: none;
   color: black;
+`;
+
+export const ImgStyle = styled.img`
+  width: 450px;
+  cursor: pointer;
+`;
+
+export const TextContent = styled.p`
+  text-align: center;
 `;
